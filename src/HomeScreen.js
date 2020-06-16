e6d2b7122be604cf205dd94ffda4f36575e75f89
nbrown0000 <brown.nathanjames@gmail.com>
2020-06-16 19:25:54 +1000

    Add HomeScreen and Login components. Added styling to Login component.

---
+++ src/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <div>
+      Home Screen
+    </div>
+  )
+}
+
+// #104891 accent colour for active list
+
+export default HomeScreen;
---
+++ src/Login.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <form className="login">
+      <p className="login__header"><b>Log in</b> to acess your notes</p>
+      <p className="login__username">
+        <label>Username:</label>
+        <input />
+      </p>
+      <p className="login__password">
+        <label>Password:</label>
+        <input />
+      </p>
+      <p className="login__submit">
+        <button className="login__submit-button">Login</button>
+      </p>
+      <div className="login__register">
+        <p className="login__register-message">Don't have an account?</p>
+        <button className="login__register-button">Register Now</button>
+      </div>
+    </form>
+  )
+}
+
+// #104891 accent colour for active list
+
+export default Login;
