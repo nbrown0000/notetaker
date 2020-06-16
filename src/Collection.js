e5393e682da3d410d83da7c39a0bff3e1b3ca3c4
nbrown0000 <brown.nathanjames@gmail.com>
2020-06-16 20:38:45 +1000

    Add mockup username and user image

--- src/Collection.js
+++ src/Collection.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import "./Collection.css";
+import userIcon from "./user_anonymous.jpg";
 
-const Collection = () => {
+const Collection = ({ username }) => {
   return (
     <div className="collection">
-      <h2 className="collection__header">[ username ]</h2>
+      <div className="collection__header">
+        <img src={userIcon} width={80} />
+        <h2 className="collection__header-title">{username}</h2>
+      </div>
     </div>
   )
 }
+
+// <a href="https://www.vecteezy.com/free-vector/profile">Profile Vectors by Vecteezy</a>
 
 export default Collection;
