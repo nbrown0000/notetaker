2d2380fb5b1465c0e9cf7a8104826bff594aef68
nbrown0000 <brown.nathanjames@gmail.com>
2020-06-21 22:10:33 +1000

    Add collection of lists. Other improvements. Added default icon to handle no image uploaded by user. Added dynamic selecting of activeList. Changed username to name to better reflect name field.

--- src/Collection.js
+++ src/Collection.js
@@ -2,12 +2,25 @@ import React from 'react';
 import "./Collection.css";
 import userIcon from "./user_anonymous.jpg";
 
-const Collection = ({ username }) => {
+const Collection = ({ name, collection, setActiveList }) => {
   return (
     <div className="collection">
       <div className="collection__header">
         <img src={userIcon} alt="User" width={80} />
-        <h2 className="collection__header-title">{username}</h2>
+        <h2 className="collection__header-title">{name}</h2>
+        <ul className="collection__list">
+          {collection.map((el,i) => {
+
+            return <li
+              key={i}
+              className="collection__item"
+              onClick={() => setActiveList(el)}
+            >
+              {el}
+            </li>
+
+          })}
+        </ul>
       </div>
     </div>
   )
