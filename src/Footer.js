dc85105e16539b13bac88c7e7595b72796e74421
nbrown0000 <brown.nathanjames@gmail.com>
2020-06-24 17:53:58 +1000

    Add Footer component. This will replace activeList component.

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = ({ activeList }) => {
+
+  return (
+    <footer className="footer">
+      Footer
+    </footer>
+  )
+}
+
+export default Footer;
