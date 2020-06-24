ddcbc2586e4af01f6ed36869828b1b30f323e0f3
nbrown0000 <brown.nathanjames@gmail.com>
2020-06-24 12:49:05 +1000

    Update to handle change in collection data structure. Removed unnecessary header

--- src/Collection.js
+++ src/Collection.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import "./Collection.css";
-import userIcon from "./user_anonymous.jpg";
 
 const Collection = ({ name, collection, setActiveList }) => {
   return (
-    <div className="collection">
-      <div className="collection__header">
-        <img src={userIcon} alt="User" width={80} />
-        <h2 className="collection__header-title">{name}</h2>
+    <section className="collection">
         <ul className="collection__list">
           {collection.map((el,i) => {
 
@@ -21,8 +17,7 @@ const Collection = ({ name, collection, setActiveList }) => {
 
           })}
         </ul>
-      </div>
-    </div>
+    </section>
   )
 }
 
