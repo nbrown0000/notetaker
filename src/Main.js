811af69babbe621b028e663128dfca5f15625518
nbrown0000 <brown.nathanjames@gmail.com>
2020-06-26 19:20:51 +1000

    Upgrade Main from stateless component to Class. This prepares for Main holding state.

--- src/Main.js
+++ src/Main.js
@@ -2,54 +2,64 @@ import React from 'react';
 import "./Main.css";
 import calendarIcon from "./icons/011-planner.png";
 
-const Main = ({ collection, activeList, onCollectionClicked }) => {
-  console.log('activeList', activeList)
+class Main extends React.Component {// = ({ collection, activeList, onCollectionClicked }) => {
+  
+  render () {
+    
+    const {
+      activeList,
+      collection,
+      onCollectionClicked
+    } = this.props;
 
-  return (
-    // collection === '' || activeList === '' ?
-    // <></>
-    // :
-    <main className="main">
+    
 
-      <div className="main__heading">
-        <h2 className="main__heading-title">Lists</h2>
-        <div className="main__heading-add">
-          <p className="main__heading-add-symbol">+</p>
-          {/* <p className="main__heading-add-text">Add List</p> */}
+    // console.log('activeList', activeList)
+
+    return (
+
+      <main className="main">
+
+        <div className="main__heading">
+          <h2 className="main__heading-title">Lists</h2>
+          <div className="main__heading-add">
+            <p className="main__heading-add-symbol">+</p>
+            {/* <p className="main__heading-add-text">Add List</p> */}
+          </div>
         </div>
-      </div>
 
-      <ul className="main__collection">
-        {collection === '' ?
-          <></> :
-        collection.map((item,i) => {
-          return (
-            <li key={i} className="main__collection-item" onClick={() => onCollectionClicked(item.id)}>
-              <img alt="item" src={calendarIcon} width="15em" />
-              {item.name}
-            </li>)
-        })}
-      </ul>
-
-      <ul className="main__activelist">
-        {activeList.length === 0 ?
-          <></>
-        :
-          activeList.map((item,i) => {
+        <ul className="main__collection">
+          {collection === '' ?
+            <></> :
+          collection.map((item,i) => {
             return (
-              <li className="main__activelist-item" key={i}>
+              <li key={i} className="main__collection-item" onClick={() => onCollectionClicked(item.id)}>
+                <img alt="item" src={calendarIcon} width="15em" />
                 {item.name}
               </li>)
-          })
-        }
-      </ul>
+          })}
+        </ul>
 
-      <section className="main__completed">
-        <ul className="main__completed-list"><li>Completed</li></ul>
-      </section>
-      
-    </main>
+        <ul className="main__activelist">
+          {activeList.length === 0 ?
+            <></>
+          :
+            activeList.map((item,i) => {
+              return (
+                <li className="main__activelist-item" key={i}>
+                  {item.name}
+                </li>)
+            })
+          }
+        </ul>
+
+        <section className="main__completed">
+          <ul className="main__completed-list"><li>Completed</li></ul>
+        </section>
+        
+      </main>
   )
+      }
 }
 
 export default Main;
