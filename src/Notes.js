afa3d6f90bf9a27e4735d14326e4b475fcb84bfe
Nathan Brown <brown.nathanjames@gmail.com>
2020-08-15 23:04:03 +1000

    Upgrade from stateless component to class component. Additional minor changes.
    Upgraded from stateless component to class component. This enables use of state.
    Added itemToAdd state variable for adding notes.
    Added input for adding notes.

--- src/Notes.js
+++ src/Notes.js
@@ -1,40 +1,56 @@
 import React from 'react';
 import "./Notes.css";
 
-const Notes = ({ activeList }) => {
+class Notes extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      itemToAdd: ''
+    }
+  }
 
-  return (
-    <section className="notes">
-      {
-      activeList.length === 0 ?
-        <>
-          No notes found!
-        </>
-      :
-        <ul className="notes__list">
-          {activeList.map((note,i) => {
-            return (
-            <li className="notes__item" key={i}>
-              {note.body}
+  render() {
+    const inputStyle = { width: "70%" }
+
+    return (
+      <section className="notes">
+        {
+          <ul className="notes__list">
+            {this.props.activeList.map((note,i) => {
+              return (
+              <li className="notes__item" key={i}>
+                {note.body}
+              </li>
+              )
+            })}
+            <li className="notes__add">
+              <input
+                style={inputStyle}
+                // onChange={this.handleItemChange}
+                placeholder="Add a Note"
+                type="text"
+              />
+              <button
+                // onClick={() => onAddItemToNote(itemToAdd)}
+              >Add</button>
             </li>
-            )
-          })}
-        </ul>
-      }
-    </section>
-    // <li className="activelist__item">
-    //   <input
-    //     className="activelist__item-checkbox"
-    //     type="checkbox"
-    //     onClick={() => onCheck(item.name)}
-    //     defaultChecked={item.isDone}
-    //   />
-    //   <span
-    //     className="activelist__item-name"
-    //     style={nameStyle}
-    //   > {item.name} </span>
-    // </li>
-  )
+          </ul>
+        }
+      </section>
+      // <li className="activelist__item">
+      //   <input
+      //     className="activelist__item-checkbox"
+      //     type="checkbox"
+      //     onClick={() => onCheck(item.name)}
+      //     defaultChecked={item.isDone}
+      //   />
+      //   <span
+      //     className="activelist__item-name"
+      //     style={nameStyle}
+      //   > {item.name} </span>
+      // </li>
+    )
+  }
 }
 
 export default Notes;
