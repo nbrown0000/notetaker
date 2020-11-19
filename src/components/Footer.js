0bcb276b77e18ef1b8e3ed2c4b557d87f450ce94
Nathan Brown <brown.nathanjames@gmail.com>
2020-11-19 22:46:38 +1100

    Change styles
    Updated styles to match new design
    Change bacground colour
    changed height to smaller

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,7 @@ import "./Footer.css"
 const Footer = () => {
 
   return (
-    <footer className="footer">
-      {/* Icons made by <a href="https://www.flaticon.com/authors/dmitri13" title="dmitri13">dmitri13</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> */}
-      {/* Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> */}
-      {/* Icons made by <a href="https://www.flaticon.com/authors/google" title="Google">Google</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> */}
-    </footer>
+    <footer className="footer"></footer>
   )
 }
 
