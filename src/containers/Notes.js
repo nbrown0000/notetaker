b4a22f34cbccb81387624666ae397106837c2df5
Nathan Brown <brown.nathanjames@gmail.com>
2021-01-15 23:41:33 +1100

    Convert Notes.js from class to function component
    Converted to function component to benefit from react hooks and simplfy the component.
    Fixed issue with title not updating when redux state updates, solution involved using dependency array.

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Notes.css";
 import Note from "../components/Note";
 import { connect } from "react-redux";
@@ -19,125 +19,105 @@ const mapStateToProps = state => {
   };
 }
 
-class ConnectedNotes extends React.Component {
-  constructor(props) {
-    super(props)
-    
-    this.state = {
-      itemToAdd: '',
-      mode: 'view',
-      color: "#FFF7B6",
-      title: this.props.notesTitle,
-      notesToUpdate: []
-    }
-  }
+function ConnectedNotes(props) {
 
-  backToLists = () => {
-    this.props.setView('lists')
-  }
+  const [mode, setMode] = useState('view');
+  const [color, setColor] = useState('#FFF7B6');
+  const [title, setTitle] = useState('');
+  React.useEffect(() => {
+    setTitle(props.notesTitle);
+  }, [props.notesTitle])
 
-  editNotes = () => {
-    this.setState({ mode: 'edit' })
-  }
 
-  onTitleInputChange = (e) => {
-    this.setState({ title: e.target.value })
-  }
-
-  updateNotes = () => {
+  const updateNotes = () => {
     // Update Title (call api to change list title)
-    this.props.updateList(this.props.notesListId, this.state.title);
-    this.props.getLists(this.props.user.user_id)
-    this.props.getNotes(this.props.notesListId);
-    this.setState({title: this.props.notesTitle})
+    // console.log(title)
+    props.updateList(props.notesListId, title);
+    props.getLists(props.user.user_id);
+    props.getNotes(props.notesListId);
+    // this.setState({title: this.props.notesTitle})
 
 
     // this.updateListTitle();
     // // this.updateListNotes();
     // this.setState({ title: store.getState().activeList.title })
-    this.setState({ mode: 'view' });
+
+    // replace with hooks
+    setMode('view')
+    // this.setState({ mode: 'view' });
   }
 
-  compareNotes = (a,b) => {
+  const compareNotes = (a,b) => {
     if(a.note_id < b.note_id) { return -1; }
     if(a.note_id > b.note_id) { return 1; }
     return 0
   }
 
-  render() {
-    // console.log(this.state.title)
-    // console.log(this.props.notesTitle)
-    console.log(this.props)
 
-
-    const BACKBUTTON =
-      <button className="notes__back" onClick={() => this.backToLists()}>
-        <img src={backIcon} alt="Back" />
-      </button>;
+  const BACKBUTTON =
+    <button className="notes__back" onClick={() => props.setView('lists')}>
+      <img src={backIcon} alt="Back" />
+    </button>;
     
-    
-    const notesStyle = { background: this.state.color };
-    const { mode } = this.state;
-    const isNewList = null;
-    // const title = this.props.notesTitle;
-    
+  const notesStyle = { background: color };
+  const isNewList = null;
 
-    return (
-      <section className="notes"  >
-        <header className="notes__nav" style={notesStyle}>
-          
-          <section className="notes__header-leftsection">
-            {this.props.window.width < 481 && BACKBUTTON}
-            {/* {isNewList===true || mode==='edit' ? */}
-            {isNewList===true || mode==='edit' ?
-              <input
-                autoFocus
-                className="notes__heading-edit"
-                defaultValue={this.props.notesTitle}
-                onChange={this.onTitleInputChange}
-              />
-            :
-              <h1 className="notes__heading">{this.props.notesTitle}</h1>
-            }
-          </section>
-
-          <section className="notes__header-rightsection">
-          {/* {isNewList || mode==='edit' ? */}
-          {mode==='edit' ?
-            <button className="notes__update" onClick={() => this.updateNotes()}>
-              <img src={tickIcon} alt="accept" />
-            </button>
+  return (
+    <section className="notes"  >
+      <header className="notes__nav" style={notesStyle}>
+        
+        <section className="notes__header-leftsection">
+          {props.window.width < 481 && BACKBUTTON}
+          {/* {isNewList===true || mode==='edit' ? */}
+          {isNewList===true || mode==='edit' ?
+            <input
+              autoFocus
+              className="notes__heading-edit"
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+            />
           :
-            <button className="notes__edit" onClick={this.editNotes}>
-              <img src={editIcon} alt="Edit" />
-            </button>
+            <h1 className="notes__heading">{title}</h1>
           }
-            <button className="notes__dotsmenu">
-              <img src={dotsIcon} alt="Dotsmenu"/>
-            </button>
-          </section>
-          
-        </header>
-        <ul className="notes__list" >
-          {this.props.notes.sort(this.compareNotes).map((note,i) => {
-            return (
-              <Note
-                note={note}
-                key={i}
-                editNote={this.props.editNote}
-                deleteNote={this.props.deleteNote}
-                saveNote={this.props.saveNote}
-                color={this.state.color}
-                mode={this.state.mode}
-                addNoteToUpdate={this.addNoteToUpdate}
-              />
-            )
-          })}
-        </ul>
-      </section>
-    )
-  }
+        </section>
+
+        <section className="notes__header-rightsection">
+        {/* {isNewList || mode==='edit' ? */}
+        {mode==='edit' ?
+          <button className="notes__update" onClick={() => updateNotes()}>
+            <img src={tickIcon} alt="accept" />
+          </button>
+        :
+          <button className="notes__edit" onClick={() => setMode('edit')}>
+            <img src={editIcon} alt="Edit" />
+          </button>
+        }
+          <button className="notes__dotsmenu">
+            <img src={dotsIcon} alt="Dotsmenu"/>
+          </button>
+        </section>
+        
+      </header>
+      <ul className="notes__list" >
+        {props.notes.sort(compareNotes).map((note,i) => {
+          return (
+            <Note
+              note={note}
+              key={i}
+              editNote={props.editNote}
+              deleteNote={props.deleteNote}
+              saveNote={props.saveNote}
+              color={color}
+              mode={mode}
+              // addNoteToUpdate={addNoteToUpdate}
+            />
+          )
+        })}
+      </ul>
+    </section>
+  )
 }
+// }
 
 const Notes = connect(
   mapStateToProps,
