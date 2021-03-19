3a8081f2117c2a27de4dd82936bf59334a11a3b8
Nathan Brown <brown.nathanjames@gmail.com>
2021-03-19 11:48:55 +1100

    Change initial state
    Initial state changed from test user to ''

--- src/store/index.js
+++ src/store/index.js
@@ -3,15 +3,18 @@ import reducer from "../reducers";
 import thunk from "redux-thunk";
 
 const initialState = {
-  // route: 'login',
-  // user: '',
-  route: 'main',
   view: 'lists',
-  user: { user_id: 37, username: "johnny5" },
-  window: { width: 0, height: 0 },
+  route: 'login',
+  user: '',
   lists: [],
-  activeList: '',
   notes: []
+  // route: 'main',
+  // view: 'lists',
+  // user: { user_id: 37, username: "johnny5" },
+  // window: { width: 0, height: 0 },
+  // lists: [],
+  // activeList: '',
+  // notes: []
 }
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
